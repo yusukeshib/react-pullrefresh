aa9eeddb1b62761760e24818fad4c299181061e9
Yusuke Shibata <shibata@fata.io>
2019-03-21 21:21:39 +0900

    update deps

--- demo/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
-import whyDidYouUpdate from 'why-did-you-update'
-
-whyDidYouUpdate(React)
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
